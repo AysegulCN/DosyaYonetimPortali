bf05c1d08e7bf8c4cc4ee78ba4711ad99be9877c
AysegulCN <aysegul70coban@gmail.com>
2026-03-15 17:10:00 +0300

    .

---
+++ DosyaYonetimPortali.API/Models/AppFile.cs
@@ -0,0 +1,20 @@
+﻿namespace DosyaYonetimPortali.API.Models
+{
+    public class AppFile
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public string? Extension { get; set; }
+        public string? PhysicalPath { get; set; }
+        public long Size { get; set; } 
+        public DateTime UploadDate { get; set; } = DateTime.Now;
+
+        // Bu dosya hangi klasörde? (Null ise ana dizindedir)
+        public int? FolderId { get; set; }
+        public Folder Folder { get; set; }
+
+        // Bu dosyayı kim yükledi?
+        public string? AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
---
+++ DosyaYonetimPortali.API/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.IO.Compression;
+
+namespace DosyaYonetimPortali.API.Models
+{
+    // IdentityUser sınıfından miras alıyoruz ki hazır giriş/çıkış/şifreleme özellikleri gelsin
+    public class AppUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        // Bir kullanıcının birden fazla klasörü ve dosyası olabilir (Bire-Çok İlişki)
+        public ICollection<Folder> Folders { get; set; }
+        public ICollection<AppFile> Files { get; set; }
+    }
+}
